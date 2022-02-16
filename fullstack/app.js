f1df6c4908b85788fde463428abe511dafe10c9a
Wladiosan <w.ohrimenko@gmail.com>
2022-02-16 05:34:57 +0200

    1/6: parsing user data (json)

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
@@ -6,6 +8,9 @@ const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
