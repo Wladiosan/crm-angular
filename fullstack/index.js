9184bb29e0ecdd4d544023bacd08b7a8d3da2e9c
Wladiosan <w.ohrimenko@gmail.com>
2022-02-16 04:51:18 +0200

    1/3: setting start server

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,7 +1,5 @@
-const express = require('express')
-
-const app = express()
-const PORT = 5000
+const app = require('./app')
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.status(200).json({
